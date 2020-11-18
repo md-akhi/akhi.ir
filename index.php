ea571f43c106cc7ea2e139998e24279e6e3e58ab
MD Amanalikhani <md.akhi.ir@gmail.com>
2020-11-18 13:27:37 +0330

    Upload Data

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+        header('Location: http://md.akhi.ir');
+?>
+
+<noscript>
+	<meta http-equiv="refresh" content="0;url=http://md.akhi.ir" />
+</noscript>
+<script type="text/javascript">
+	window.location.href="http://md.akhi.ir"
+</script>
